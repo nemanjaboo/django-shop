bcb11336e8a885b22d2c4eadda5e9e46651f2261
nemanja.novakovic <nemanjalfc89@gmail.com>
2022-11-30 08:26:11 +0100

    added players to Game model

--- shop/models.py
+++ shop/models.py
@@ -16,6 +16,7 @@ class Game(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=7)
     category = models.ManyToManyField(Category)
     status = models.BooleanField(default=0)
+    players = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
