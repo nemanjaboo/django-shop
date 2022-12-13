8022fe9394626292c72b7235a92e4c99cd97bd8b
nemanja.novakovic <nemanjalfc89@gmail.com>
2022-12-13 16:16:25 +0100

    added styles and materialize

--- shop/urls.py
+++ shop/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('<slug:slug>', views.GameDetailView.as_view(), name='game-detail'),
-    path('games/', views.GameListView.as_view(), name='games-list')
+    path('', views.GameListView.as_view(), name='games-list')
 ] 
 
